7482114abe0e09820057347deb07d0000e7a3461
Antonis Skandalis <ant.skandalis@gmail.com>
2021-04-07 23:59:54 +0300

    chore: add basic packages and setup linting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('will render the app', () => {
+    render(<App />);
+
+    const baseText = screen.getByText(/Crypto Track App/i);
+    expect(baseText).toBeInTheDocument();
+  });
 });
